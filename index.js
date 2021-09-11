af6e77fc438ad8bd6a0ba9aa48f27f5f4afae995
Wasanta <kwan_u_day@hotmail.com>
2021-09-11 10:04:06 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+    main.remove();
+    const newHeader = document.createElement("h1");
+    newHeader.setAttribute("id", "victory");
+    // document.getElementById("victory");
+    // document.innerHTML = "Wasanta is the champion";
+    // const text = document.getElementById("victory");
+    newHeader.innerHTML="<h1>Wasanta is the champion</h1>";
+    
+    
